7d834ce9d090e36d80e6b22d932d732718027a05
ponta99991 <plm18001@student.mdh.se>
2020-02-28 11:21:59 +0100

    Created object and translate func

--- Deep-N-Dark-Game/main.cpp
+++ Deep-N-Dark-Game/main.cpp
@@ -1,15 +1,32 @@
 #include "SDL.h"
 #include "game.h"
+#include "object.h"
 
 
 int main(int argc, char* argv[])
 {
 
-	Game window("Dark-N-Deep", 300, 200, 800, 600);
+	std::cout << "game has started\n";
+	Game game("Dark-N-Deep", 300, 200, 800, 600);
+	Object o(400, 300, 20, 20);
 
-	while (window.running()) {
-		window.handleEvents();
-		window.update();
+	while (game.running()) {
+		if (game._downKeyPressed) {
+			o.translate(down, 1);
+		}
+		else if (game._leftKeyPressed) {
+			o.translate(left, 1);
+		}
+		else if (game._upKeyPressed) {
+			o.translate(up, 1);
+		}
+		else if (game._rightKeyPressed) {
+			o.translate(right, 1);
+		}
+		game.handleEvents();
+		//game.update();
+		o.draw();
+		game.render();
 	}
 	return 0;
 }
