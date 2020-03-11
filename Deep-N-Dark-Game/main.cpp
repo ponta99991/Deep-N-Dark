917ef736302e4d53c4e2baf1235faf6a31d7d7be
ponta99991 <pontus.lidholm@telia.com>
2020-03-11 21:46:56 +0100

    Fps cap

--- Deep-N-Dark-Game/main.cpp
+++ Deep-N-Dark-Game/main.cpp
@@ -5,12 +5,19 @@
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+	Uint32 frameStart;
+	int frameTime;
 
 	std::cout << "game has started\n";
 	Game game("Dark-N-Deep", 300, 200, 800, 600);
 	Object o(400, 300, 20, 20);
 
 	while (game.running()) {
+		frameStart = SDL_GetTicks();
+		game.handleEvents();
+
 		if (game._downKeyPressed) {
 			o.translate(down, 1);
 		}
@@ -23,10 +30,14 @@ int main(int argc, char* argv[])
 		else if (game._rightKeyPressed) {
 			o.translate(right, 1);
 		}
-		game.handleEvents();
 		//game.update();
 		o.draw();
 		game.render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);//Waiting until 1000/fps time has passed
+		}
 	}
 	return 0;
 }
